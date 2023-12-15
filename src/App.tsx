08fc85e62399e9d3496cd06c75d86dd929baa76d
Kiri1111 <92_medved@mail.ru>
2023-12-15 12:01:54 +0300

    feat: first step checkBox

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Button } from '@/ui/button'
+import { CheckBox } from '@/ui/checkBox'
 import { Header } from '@/ui/header'
 
 export function App() {
   return (
     <div>
       <Header />
+      <CheckBox />
       <Button as={'button'}>{'ddd'}</Button>
       <Button as={'a'}>{'dddssdd'}</Button>
     </div>
